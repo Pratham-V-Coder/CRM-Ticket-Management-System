e7a999d609fdfe34690204abf59e96c34fc0380a
Pratham <prathamverma8880@gmail.com>
2026-08-11 10:43:56 +0530

    Fix React Router routes on Vercel

--- Frontend/CRMfrontend/src/api/ticketApi.js
+++ Frontend/CRMfrontend/src/api/ticketApi.js
@@ -7,6 +7,7 @@ import {
 const rootUrl = import.meta.env.VITE_API_URL
   ? `${import.meta.env.VITE_API_URL}/`
   : "http://localhost:4000/v1/";
+
 const ticketUrl = rootUrl + "ticket";
 const closeTicketUrl = rootUrl + "ticket/close-ticket/";
 
